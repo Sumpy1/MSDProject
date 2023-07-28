b6337899a8ec9efd45ebc323834657de92d7bf54
Sumiran Pokharel <130939290+Sumpy1@users.noreply.github.com>
2023-07-28 17:32:01 -0400

    seconds elapsed added for easier interpolation
    Need to modify code for upsizing number of data to match with lidar data and add interpolation code

--- HC-SR04 Distance sensor/Distance_sensor.py
+++ HC-SR04 Distance sensor/Distance_sensor.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import time
 import csv
 import os
+from datetime import datetime
 
 GPIO.setmode(GPIO.BCM)
 
@@ -15,10 +16,10 @@ GPIO.setup(ECHO, GPIO.IN)
 
 def get_distance():
     GPIO.output(TRIG, False)
-    time.sleep(0.0000000001)
+    time.sleep(0.00000000000000000001)
 
     GPIO.output(TRIG, True)
-    time.sleep(0.00000001)
+    time.sleep(0.00000000001)
     GPIO.output(TRIG, False)
 
     pulse_start = time.time()
@@ -36,27 +37,37 @@ def get_distance():
 
     return distance
 
+def timestamp_to_seconds(timestamp_str):
+    # Parse the timestamp string to a datetime object
+    time_obj = datetime.strptime(timestamp_str, "%H:%M:%S.%f")
+
+    # Calculate the total number of seconds, including the fractional part
+    total_seconds = time_obj.hour * 3600 + time_obj.minute * 60 + time_obj.second + time_obj.microsecond / 1e6
+
+    return total_seconds
+
 try:
     desktop_path = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
     filename = os.path.join(desktop_path, "Distance_data.csv")
 
     with open(filename, mode='w', newline='') as file:
         csv_writer = csv.writer(file)
-        csv_writer.writerow(["Timestamp", "Distance (cm)"])
+        csv_writer.writerow(["Regular Timestamp", "Timestamp (Number)", "Distance (cm)"])
 
         start_time = time.time()
 
         while True:
             distance = get_distance()
 
-            # Get the current timestamp with three decimal points for milliseconds
-            current_time = time.time()
-            formatted_time = time.strftime("%H:%M:%S", time.localtime(current_time))
-            timestamp_ms = round((current_time - int(current_time)) * 1000)
-            current_time = f"{formatted_time}.{timestamp_ms:03}"
+            # Get the regular timestamp with three decimals for seconds
+            regular_timestamp = datetime.now().strftime("%H:%M:%S.%f")[:-3]
 
-            csv_writer.writerow([current_time, distance])
-            print(f" {distance} cm, {current_time}")
+            # Get the current timestamp as a number representing seconds elapsed in the day
+            current_time_str = datetime.now().strftime("%H:%M:%S.%f")
+            current_time = timestamp_to_seconds(current_time_str)
+
+            csv_writer.writerow([regular_timestamp, current_time, distance])
+            print(f"{regular_timestamp}, {current_time}, {distance}")
 
 except KeyboardInterrupt:
     print("Measurement stopped by the user")
